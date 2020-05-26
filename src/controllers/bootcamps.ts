49d6d08e40eb03f6ede339f39e115c88ef33c84c
beqa <beqasirbiladze1@gmail.com>
2020-05-27 00:22:08 +0400

    add colors for console add morgan loger

--- src/controllers/bootcamps.ts
+++ src/controllers/bootcamps.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "../types";
+import { Color } from "~/globals";
+
 
 class BootcampsController {
 
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,24 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import bootcamps from './router/bootcamps';
-
+import { api } from './globals';
+import morgan from 'morgan';
 
 dotenv.config({ path: './config/config.env' });
-const app: Express = express();
 
 const PORT = process.env.PORT || 5000;
+const app: Express = express();
 
-export const api = '/api/v1';
+//dev log middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
+//routes
 app.use(`${api}/bootcamps`, bootcamps);
 
 
+//start server
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 });
-;
